1da64cdcd8cda1d6cfb52c1252ef3ea07b69e410
wenrui <macomfan@163.com>
2020-04-19 19:02:22 +0800

    small change

--- producer/src/main/java/cn/ma/cei/test/websocket/request/RequestHandler.java
+++ producer/src/main/java/cn/ma/cei/test/websocket/request/RequestHandler.java
@@ -1,8 +1,6 @@
 package cn.ma.cei.test.websocket.request;
 
-import cn.ma.cei.generator.buildin.WebSocketConnection;
 import cn.ma.cei.service.IWebSocketHandler;
-import cn.ma.cei.service.WebSocketMessage;
 import cn.ma.cei.service.WebSocketService;
 import cn.ma.cei.test.websocket.messages.EchoMessage;
 import io.vertx.core.buffer.Buffer;
